a4a843f3f0cd5e7f5c9aa0daf4f08e5359e90a47
Loïck Virot <virot.loick@gmail.com>
2016-06-02 02:14:51 +0200

    Update phptool_create.sh

--- home/www/phptool_create.sh
+++ home/www/phptool_create.sh
@@ -2,13 +2,13 @@
 
 if [ -d /home/loick/www/$1 ]
 then
-  echo "The folder \"$1\" already exists"
+  echo "***[Error]*** Project \"$1\" already exists"
 else
   #Create project folder
   `mkdir /home/loick/www/$1`
   if [ -d /home/loick/www/$1 ]
   then
-    echo "Folder \"$1\" created."
+    echo "[Info] Folder \"$1\" created."
   else
     echo "***[Error]*** Project folder creation failed"
   fi
@@ -16,7 +16,7 @@ else
   `mkdir /home/loick/www/$1/log`
   if [ -d /home/loick/www/$1/log ]
   then
-    echo "Log Folder created."
+    echo "[Info] Log Folder created."
   else
     echo "***[Error]*** Log folder creation failed"
   fi
@@ -25,7 +25,7 @@ else
   DocumentRoot \"/home/loick/www/$1\"
   ServerName $1.dev
 
-  <Directory /home/loick/www/myproject>
+  <Directory /home/loick/www/$1>
     Options FollowSymLinks
     AllowOverride all
     Require all granted
@@ -37,7 +37,7 @@ else
   " > /home/loick/www/sites-available/$1.dev.conf
   if [ -f /home/loick/www/sites-available/$1.dev.conf ]
   then
-    echo "Configuration file created in /home/loick/www/sites-available"
+    echo "[Info] Configuration file created in /home/loick/www/sites-available"
   else
     echo "***[Error]*** Configuration file creation failed"
   fi
@@ -47,12 +47,12 @@ else
   " > /home/loick/www/$1/index.php
   if [ -f /home/loick/www/$1/index.php ]
   then
-    echo "index.php created"
+    echo "[Info] index.php created"
     chmod 775 /home/loick/www/$1/index.php
   else
     echo "***[Error]*** index.php creation failed"
   fi
   #restart apache
-  echo "Restarting apache..."
+  echo "[Info] Restarting apache..."
   systemctl restart httpd.service
 fi
